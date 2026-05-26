dde8375f579090fabbc7839066ea310ac48942f9
Melany200415 <melany.perugachi@epn.edu.ec>
2026-05-25 21:52:29 -0500

    Implement validation methods in Validador class
    Added methods to validate email, phone, text, and age.

--- TareaIntegradoraa/src1/src/Util/Validador.java
+++ TareaIntegradoraa/src1/src/Util/Validador.java
@@ -1,4 +1,27 @@
 package Util;
 
 public class Validador {
+
+    public static boolean validarCorreo(String correo) {
+
+        return correo != null
+                && correo.contains("@")
+                && correo.contains(".");
+    }
+
+    public static boolean validarTelefono(String telefono) {
+
+        return telefono.matches("\\d+");
+    }
+
+    public static boolean validarTexto(String texto) {
+
+        return texto != null
+                && !texto.trim().isEmpty();
+    }
+
+    public static boolean validarEdad(int edad) {
+
+        return edad > 0 && edad < 150;
+    }
 }
